815cc7901fc0caf6e659898eb50bb84b4b29b7ca
silajitpass12 <silajitk.samal@gmail.com>
2017-08-10 15:57:59 +0530

    add new drupal 8 custom module

---
+++ site_api/src/Controller/SiteApiController.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\site_api\Controller\SiteApiController.
+ */
+
+namespace Drupal\site_api\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\Entity\Node;
+use Drupal\field\FieldConfigInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+class SiteApiController extends ControllerBase {
+
+      public function getJsonbyNodeId($sitekey,$nid) {
+        
+        if (!empty($nid) && !empty($sitekey)) {
+          $siteapikey = \Drupal::config('site_api.settings')->get('siteapikey');
+          $node = Node::load($nid);
+          $output = array();
+          // the rest of your code here
+          if(!empty($node) && !empty($siteapikey) && ($sitekey == $siteapikey)) {
+            foreach($node as $id => $val) {
+              $output[$id] = $node->get($id)->getValue();
+            }
+            $type =  $node->getType();
+            if($type == 'page') {
+              return new JsonResponse(
+                array(
+                  'node' => $output,
+                  'type' => $type,
+                )
+              );
+            }
+            else {
+              throw new AccessDeniedHttpException();
+            }
+          }
+        else {
+          throw new AccessDeniedHttpException();
+          
+        }
+    
+  
+  }
+  else {
+    throw new AccessDeniedHttpException();
+    
+  }
+
+}   
+
+}
+
